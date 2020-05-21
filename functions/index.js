11162f99e37e87de4d7c9b14f69a1e6551f112d2
joao-assuncao <47530467+joao-assuncao@users.noreply.github.com>
2020-05-21 17:23:37 -0300

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -8,28 +8,41 @@ admin.initializeApp();
 
 const db = admin.firestore().collection("usuarios");
 
-app.get("/usuarios", (req, res)=>{
-
-	return db.get()
-		.then((docs)=>{
+app.get("/users", (request, response) => {
+	return db.get().then((docs) => {
 		let users = [];
-
-		docs.forEach((docs)=>{
-			users.push({id: doc.id, nome: doc.data().name});
+		docs.forEach((doc) => {
+			users.push({
+				name: doc.data().name,
+				birth: doc.data().birth,
+				dateRegister: doc.data().dateRegister,
+				foot: doc.data().foot,
+				height: doc.data().height,
+				imgBg: doc.data().imgBg,
+				imgPerson: doc.data().imgPerson,
+				lastName: doc.data().lastName,
+				nick: doc.data().nick
+			})
 		})
+		return response.json(users);
+	});
+})
 
-		//response.json(users);
-		return users;
-
+app.post("/user", (request, response) => {
+	return db.add({ 
+		birth: request.body.birth,
+		dateRegister: request.body.dateRegister,
+		foot: request.body.foot,
+		height: request.body.height,
+		imgBg: request.body.imgBg,
+		imgPerson: request.body.imgPerson,
+		lastName: request.body.lastName,
+		name: request.body.name,
+		nick: request.body.nick
 	})
-
-});
+		.then(() => {
+			return response.json(null);
+		})
+})
 
 exports.api = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
